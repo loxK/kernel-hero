3dd69b6578a1ff599be969ed1896beca7400d5ad
Alex Boag-Munroe <boagenator@gmail.com>
2010-07-19 23:27:49 +0100

    Fixed so kernel obeys CONFIG_MSM_CPU_FREQ_ONDEMAND_MIN and CONFIG_MSM_CPU_FREQ_ONDEMAND_MAX at boot time.

--- arch/arm/mach-msm/cpufreq.c
+++ arch/arm/mach-msm/cpufreq.c
@@ -91,13 +91,14 @@ static int __init msm_cpufreq_init(struct cpufreq_policy *policy)
 	struct cpufreq_frequency_table *table =
 		cpufreq_frequency_get_table(smp_processor_id());
 
-	BUG_ON(cpufreq_frequency_table_cpuinfo(policy, table));
 	policy->cur = acpuclk_get_rate();
-#if 0
-	/* restrict cpu freq scaling range by overwriting */
+	if (cpufreq_frequency_table_cpuinfo(policy, table)) {
+		policy->cpuinfo.min_freq = CONFIG_MSM_CPU_FREQ_ONDEMAND_MIN;
+		policy->cpuinfo.max_freq = CONFIG_MSM_CPU_FREQ_ONDEMAND_MAX;
+	}
 	policy->min = CONFIG_MSM_CPU_FREQ_ONDEMAND_MIN;
 	policy->max = CONFIG_MSM_CPU_FREQ_ONDEMAND_MAX;
-#endif
+
 	policy->cpuinfo.transition_latency =
 		acpuclk_get_switch_time() * NSEC_PER_USEC;
 	return 0;
@@ -115,7 +116,7 @@ static struct cpufreq_driver msm_cpufreq_driver = {
 	.verify		= msm_cpufreq_verify,
 	.target		= msm_cpufreq_target,
 	.name		= "msm",
-	.attr           = msm_cpufreq_attr,
+        .attr           = msm_cpufreq_attr,
 };
 
 static int __init msm_cpufreq_register(void)
